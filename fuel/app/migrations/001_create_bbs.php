a769b78e588048bea05ae57dc7025219c23364c6
kenjis <kenji.uui@gmail.com>
2013-07-19 20:51:29 +0900

    Fix migration file

--- fuel/app/migrations/001_create_bbs.php
+++ fuel/app/migrations/001_create_bbs.php
@@ -7,13 +7,12 @@ class Create_bbs
 	public function up()
 	{
 		\DBUtil::create_table('bbs', array(
-			'id' => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
-			'post_date' => array('type' => 'date'),
-			'message' => array('constraint' => 100, 'type' => 'varchar'),
-			'created_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
-			'updated_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
-
-		), array('id'));
+			'id' => array('type' => 'integer primary key', 'autoincrement' => true),
+			'post_date' => array('type' => 'text'),
+			'message' => array('type' => 'text'),
+			'created_at' => array('type' => 'integer', 'null' => true),
+			'updated_at' => array('type' => 'integer', 'null' => true),
+		));
 	}
 
 	public function down()
